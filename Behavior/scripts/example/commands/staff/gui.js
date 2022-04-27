3cfbd6be98a4527314b52304b9114d27409a1d1c
FrostIce482 <frostice48260@gmail.com>
2022-04-28 00:41:50 +0700

    slight fix

--- Behavior/scripts/example/commands/staff/gui.js
+++ Behavior/scripts/example/commands/staff/gui.js
@@ -203,6 +203,7 @@ const setModule = (plr, module, newValue) => {
     const objdata = obj(module.obj[0]).dummies
     newValue ??= ( ( objdata.get(module.name) ?? 0 ) + 1 ) % module.toggle.length
     objdata.set(module.name, newValue)
+    obj(module.obj[1]).dummies.set(module.name, newValue)
     tellrawStaff(`§¶§cUAC ► §bPlayer §d${plr.name}§b has set the module §e${module.mname}§b to ${module.toggle[newValue]}`)
 }
 
