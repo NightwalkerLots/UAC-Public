77c969472bdefc8782a2aa4ff7d2faf4af6bc211
FrostIce482 <frostice48260@gmail.com>
2022-11-30 15:10:53 +0700

    wrap runCommandAsync to bypass queue limit

--- Behavior/scripts/library/utils/cmd_queue.js
+++ Behavior/scripts/library/utils/cmd_queue.js
@@ -1,27 +1,28 @@
-import { Dimension, Enchantment, Entity, Player, system, world } from "@minecraft/server";
+import { Dimension, Entity, Player, system, world } from "@minecraft/server";
 import { SimulatedPlayer } from "@minecraft/server-gametest";
 import { getStackTrace } from "./misc.js";
 export const overworld = world.getDimension('overworld');
 
-let commandQueueCount = 0
-
+// kept for compability - too lazy to change everything on scoreboard.js
 export async function asyncExecCmd(command, source = overworld) {
-    if (commandQueueCount > 127) console.warn(`Command queue overload! (${commandQueueCount})`)
-    while (true) {
-        try {
-            return source.runCommandAsync(command);
-        } catch (error) {
-            //console.warn(error, error.stack);
-            await new Promise(res => system.run(res, 0))
-            source.runCommandAsync(command);
-        }
-    }
+    return source.runCommandAsync(command)
 }
+
+let queueCount = 1
 
 for (const cl of [Dimension, Entity, Player, SimulatedPlayer]) {
     const ORC = cl.prototype.runCommandAsync
-    cl.prototype.runCommandAsync = function(c) {
-        console.warn(`[${cl.name}/${this.id}] Run command: ${c} \n${getStackTrace(1)}`)
-        return ORC(c)
+    cl.prototype.runCommandAsync = async function(c) {
+        if (++queueCount > 127) console.warn(`Command queue overload! (${queueCount}) \n${getStackTrace(1)}`)
+        console.warn(`[${cl.name}/${this.id}] Run command (queue count: ${queueCount}): ${c} \n${getStackTrace(1)}`)
+
+        while (true) {
+            try {
+                return ORC(command).finally(() => queueCount--);
+            } catch (error) {
+                await new Promise(res => system.run(res, 0))
+                source.runCommandAsync(command);
+            }
+        }
     }
 }
