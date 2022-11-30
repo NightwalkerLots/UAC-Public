73fa312c90c957d761eb0cd9d4866a3e53dd288a
FrostIce482 <frostice48260@gmail.com>
2022-11-30 15:31:08 +0700

    moved runCommandAsync wrap to wrap.js

--- Behavior/scripts/example/wrap.js
+++ Behavior/scripts/example/wrap.js
@@ -1,7 +1,28 @@
 import { SimulatedPlayer } from "@minecraft/server-gametest";
-import { Block, Entity, IEntityComponent, ItemCooldownComponent, ItemDurabilityComponent, ItemEnchantsComponent, ItemFoodComponent, ItemStack, Player } from "@minecraft/server";
+import { Block, Entity, IEntityComponent, ItemCooldownComponent, ItemDurabilityComponent, ItemEnchantsComponent, ItemFoodComponent, ItemStack, Player, Dimension, system } from "@minecraft/server";
+import { getStackTrace } from "../library/utils/misc.js";
 
 for (const obj of [Entity, Player, SimulatedPlayer, Block, ItemStack, IEntityComponent, Block, ItemFoodComponent, ItemCooldownComponent, ItemEnchantsComponent, ItemDurabilityComponent]) {
     const { get: OIdGet } = Object.getOwnPropertyDescriptor(obj.prototype, "typeId") ?? {};
     Object.defineProperty(obj.prototype, "id", { get: OIdGet });
+}
+
+let queueCount = 1
+
+for (const cl of [Dimension, Entity, Player, SimulatedPlayer]) {
+    console.warn(cl.name, cl.prototype)
+    const ORC = cl.prototype.runCommandAsync
+    cl.prototype.runCommandAsync = async function(c) {
+        if (++queueCount > 127) console.warn(`Command queue overload! (${queueCount}) \n${getStackTrace(1)}`)
+        console.warn(`[${cl.name}/${this.id}] Run command (queue count: ${queueCount}): ${c} \n${getStackTrace(1)}`)
+
+        while (true) {
+            try {
+                return ORC(c).finally(() => queueCount--);
+            } catch (error) {
+                await new Promise(res => system.run(res, 0))
+                this.runCommandAsync(c);
+            }
+        }
+    }
 }
