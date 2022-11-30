23ad5a9e742b3d1aeea09f43b42beca1eab3bd88
FrostIce482 <frostice48260@gmail.com>
2022-11-30 15:05:27 +0700

    Added logging for running command

--- Behavior/scripts/library/utils/cmd_queue.js
+++ Behavior/scripts/library/utils/cmd_queue.js
@@ -1,17 +1,27 @@
-import { system, world } from "@minecraft/server";
+import { Dimension, Enchantment, Entity, Player, system, world } from "@minecraft/server";
+import { SimulatedPlayer } from "@minecraft/server-gametest";
+import { getStackTrace } from "./misc.js";
 export const overworld = world.getDimension('overworld');
 
 let commandQueueCount = 0
 
 export async function asyncExecCmd(command, source = overworld) {
-    if (++commandQueueCount > 127) console.warn(`Command queue overload! (${commandQueueCount})`)
+    if (commandQueueCount > 127) console.warn(`Command queue overload! (${commandQueueCount})`)
     while (true) {
         try {
-            return source.runCommandAsync(command).finally(() => commandQueueCount--);
+            return source.runCommandAsync(command);
         } catch (error) {
             //console.warn(error, error.stack);
             await new Promise(res => system.run(res, 0))
             source.runCommandAsync(command);
         }
+    }
+}
+
+for (const cl of [Dimension, Entity, Player, SimulatedPlayer]) {
+    const ORC = cl.prototype.runCommandAsync
+    cl.prototype.runCommandAsync = function(c) {
+        console.warn(`[${cl.name}/${this.id}] Run command: ${c} \n${getStackTrace(1)}`)
+        return ORC(c)
     }
 }
