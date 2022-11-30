58cf0791a064d266209bce84c26ca7e3b445c3bd
FrostIce482 <frostice48260@gmail.com>
2022-11-30 14:49:43 +0700

    rework queue limit bypass, removed unused stuff

--- Behavior/scripts/library/async_fix/cmd_queue.js
+++ Behavior/scripts/library/async_fix/cmd_queue.js
@@ -1,16 +1,16 @@
-import { world } from "@minecraft/server";
-import server from "./server_sync.js"
+import { system, world } from "@minecraft/server";
 export const overworld = world.getDimension('overworld');
 
+let commandQueueCount = 0
 
 export async function asyncExecCmd(command, source = overworld) {
+    if (++commandQueueCount > 127) console.warn(`Command queue overload! (${commandQueueCount})`)
     while (true) {
         try {
-            return source.runCommandAsync(command);
-        }
-        catch (error) {
+            return source.runCommandAsync(command).finally(() => commandQueueCount--);
+        } catch (error) {
             //console.warn(error, error.stack);
-            await server.nextTick;
+            await new Promise(res => system.run(res, 0))
             source.runCommandAsync(command);
         }
     }
